03ba79af9016143f626146c093a72adbf42590fe
tomskopek <tomskopek@gmail.com>
2021-10-10 01:05:40 -0400

    update symlinks

--- dotfiles.sh
+++ dotfiles.sh
@@ -3,9 +3,10 @@
 # Git pull dotfiles and symlink them:
 
 git clone https://github.com/tomskopek/dotfiles.git ~/dev/
-ln -s ~/dev/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dev/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dev/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dev/dotfiles/.ideavimrc ~/.ideavimrc
 ln -s ~/dev/dotfiles/.tmux.conf ~/.tmux.conf
-ln -s ~/dev/personal/dotfiles/.ideavimrc ~/.ideavimrc
+ln -s ~/dev/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dev/dotfiles/.zshrc-mac ~/.zshrc-mac
 
 # https://neovim.io/doc/user/nvim.html#nvim-from-vim
