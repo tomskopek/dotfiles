5269a8703c004b1b392750d566f1d3e6eb25d2ae
tomskopek <tomskopek@gmail.com>
2025-02-19 08:00:51 -0500

    (nvim) add smear-cursor plugin

---
+++ .config/nvim/lua/plugins/smear-cursor.lua
@@ -0,0 +1,9 @@
+return {
+  "sphamba/smear-cursor.nvim",
+  opts = {
+    smear_between_buffers = false,
+    smear_between_neighbor_lines = false,
+    legacy_computing_symbols_support = false,
+    smear_insert_mode = false,
+  },
+}
