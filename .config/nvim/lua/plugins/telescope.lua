f7073e0246da15e557a2ec2c8ff57177fa4b5cfc
tomskopek <tomskopek@gmail.com>
2025-02-19 09:19:47 -0500

    (nvim) remove trailing whitespace

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,7 +1,7 @@
 -- Fuzzy Finder
 return {
-  'nvim-telescope/telescope.nvim', 
-  dependencies = { 
+  'nvim-telescope/telescope.nvim',
+  dependencies = {
     'nvim-lua/plenary.nvim',
     { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' } -- Use fzf for fuzzy matching
   },
