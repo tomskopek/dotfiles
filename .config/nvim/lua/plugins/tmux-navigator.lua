1d86fd152b74237daab9c145ebf6ff54625090b8
tomskopek <tomskopek@gmail.com>
2025-02-22 08:49:40 -0500

    (nvim) fix toggleterm/tmux ctr+\ conflict

--- .config/nvim/lua/plugins/tmux-navigator.lua
+++ .config/nvim/lua/plugins/tmux-navigator.lua
@@ -1,6 +1,15 @@
 -- TMUX integration
 return {
   {
-    "christoomey/vim-tmux-navigator"
+    "christoomey/vim-tmux-navigator",
+    init = function()
+      vim.g.tmux_navigator_no_mappings = 1
+
+      -- Manually set keybindings, excluding Ctrl+\
+      vim.keymap.set("n", "<C-h>", ":TmuxNavigateLeft<CR>", { silent = true })
+      vim.keymap.set("n", "<C-j>", ":TmuxNavigateDown<CR>", { silent = true })
+      vim.keymap.set("n", "<C-k>", ":TmuxNavigateUp<CR>", { silent = true })
+      vim.keymap.set("n", "<C-l>", ":TmuxNavigateRight<CR>", { silent = true })
+    end,
   },
 }
--- .config/nvim/lua/plugins/toggleterm.lua
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -2,19 +2,33 @@ return {
   {
     'akinsho/toggleterm.nvim',
     version = "*",
-    opts = {
-      open_mapping = [[<C-t>]],
-      direction = "float"
-    },
     config = function ()
-      local Terminal = require("toggleterm.terminal").Terminal
-      local lazygit = Terminal:new({ cmd = "lazygit", display_name = "lazygit", hidden = true, direction = "float" })
+      require("toggleterm").setup({
+        open_mapping = [[<c-\>]],
+        direction = "float",
+        close_on_exit = true,
+      })
 
+      local Terminal = require("toggleterm.terminal").Terminal
+
+      local lazygit = Terminal:new({ cmd = "lazygit", display_name = "lazygit", hidden = true, direction = "float" })
       function _LAZYGIT_TOGGLE()
         lazygit:toggle()
       end
-
       vim.api.nvim_set_keymap("n", "<leader>lg", "<cmd>lua _LAZYGIT_TOGGLE()<CR>", { noremap = true, silent = true, desc = "Toggle lazygit" })
+
+      local python = Terminal:new({ cmd = "python", display_name = "python REPL", hidden = true, direction = "float" })
+      function _PYTHON_TOGGLE()
+        python:toggle()
+      end
+      vim.api.nvim_set_keymap("n", "<leader>lp", "<cmd>lua _PYTHON_TOGGLE()<CR>", { noremap = true, silent = true, desc = "Toggle python" })
+
+      local ncdu = Terminal:new({ cmd = "ncdu", display_name = "ncdu", hidden = true, direction = "float" })
+      function _NCDU_TOGGLE()
+        ncdu:toggle()
+      end
+      vim.api.nvim_set_keymap("n", "<leader>ln", "<cmd>lua _NCDU_TOGGLE()<CR>", { noremap = true, silent = true, desc = "Toggle ncdu" })
+
     end,
   }
 }
