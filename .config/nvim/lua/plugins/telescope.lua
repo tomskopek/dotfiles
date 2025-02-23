87694dfa2a9b558842ebecae4307a78ba890eb2b
tomskopek <tomskopek@gmail.com>
2025-02-23 16:12:51 -0500

    (nvim) telescope: add search command history; better desc

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -6,12 +6,13 @@ return {
     { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' } -- Use fzf for fuzzy matching
   },
   keys = {
-    { "<C-p>", '<cmd>Telescope find_files<cr>', desc = 'File search' },
-    { "<C-b>", '<cmd>Telescope buffers<cr>', desc = 'Search buffers' },
-    { "<leader>sh", '<cmd>Telescope help_tags<cr>', desc = 'Search docs' },
+    { "<C-p>", '<cmd>Telescope find_files<cr>', desc = '[S]earch [F]iles' },
+    { "<C-b>", '<cmd>Telescope buffers<cr>', desc = '[S]earch [B]uffers' },
     { "\\", '<cmd>Telescope live_grep<cr>', desc = 'Grep search' },
-    { "<leader>sj", '<cmd>Telescope jumplist<cr>', desc = 'Search jumplist' },
-    { "<leader>sk", '<cmd>Telescope keymaps<cr>', desc = 'Search keymaps' },
+    { "<leader>sh", '<cmd>Telescope help_tags<cr>', desc = '[S]earch [H]elp' },
+    { "<leader>sj", '<cmd>Telescope jumplist<cr>', desc = '[S]earch [J]umplist' },
+    { "<leader>sk", '<cmd>Telescope keymaps<cr>', desc = '[S]earch [K]eymaps' },
+    { "<leader>sc", '<cmd>Telescope command_history<cr>', desc = '[S]earch [C]ommand History' },
   },
   config = function()
     local telescope = require("telescope")
