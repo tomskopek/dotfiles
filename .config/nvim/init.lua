1fc4d3ab54f1f40112089f23a5cf3bb6cc9b566e
tomskopek <tomskopek@gmail.com>
2025-02-23 16:11:34 -0500

    (nvim) add some ideas to TODO

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -41,6 +41,7 @@ vim.cmd.colorscheme("kanagawa")
 -- Copilot
 -- snippets - eg: expand ipdb, console.log, function() {}, etc etc
 
+
 -- Nice to haves:
 -- -------------
 -- Autocomplete braces
@@ -48,3 +49,19 @@ vim.cmd.colorscheme("kanagawa")
 -- Documentation for word under cursor with ctrl+k
 -- there's a bit of lag when lsp servers starting up?
 -- Update dotfiles readme for setting up a new laptop
+
+
+-- Try these out:
+-- --------------
+--
+-- Credit: https://github.com/nvim-lua/kickstart.nvim/blob/master/init.lua#L166-L167
+-- Diagnostic keymaps
+-- vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+-- Credit: https://github.com/nvim-lua/kickstart.nvim/blob/master/init.lua#L169-L175
+-- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
+-- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
+-- is not what someone will guess without a bit more experience.
+-- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
+-- or just use <C-\><C-n> to exit terminal mode
+-- vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
