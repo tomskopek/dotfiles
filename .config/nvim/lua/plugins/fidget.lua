5ba0842b4d3e1f37361d33b3af3705115c3df060
tomskopek <tomskopek@gmail.com>
2025-02-25 13:11:56 -0500

    (nvim) add fidget as its own plugin

---
+++ .config/nvim/lua/plugins/fidget.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "j-hui/fidget.nvim",
+    opts = {
+      progress = {
+        display = {
+          done_ttl = 8,
+        },
+      },
+      notification = {
+        override_vim_notify = true,
+      },
+    },
+  },
+}
