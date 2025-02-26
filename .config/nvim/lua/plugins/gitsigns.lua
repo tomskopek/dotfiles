5ddacb8746675b92e3d2a25f418cdb9594e888ca
tomskopek <tomskopek@gmail.com>
2025-02-26 11:04:23 -0500

    (nvim) luastyle on gitsigns

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -1,11 +1,11 @@
 return {
   {
-    'lewis6991/gitsigns.nvim',
+    "lewis6991/gitsigns.nvim",
     enabled = true,
-    event = { 'BufReadPre', 'BufNewFile' },
+    event = { "BufReadPre", "BufNewFile" },
     config = function()
       require("gitsigns").setup({
-        on_attach = function (bufnr)
+        on_attach = function(bufnr)
           local gitsigns = require("gitsigns")
 
           local function map(mode, l, r, opts)
@@ -15,31 +15,31 @@ return {
           end
 
           -- Navigation
-          map('n', ']c', function()
+          map("n", "]c", function()
             if vim.wo.diff then
-              vim.cmd.normal({']c', bang = true})
+              vim.cmd.normal({ "]c", bang = true })
             else
-              gitsigns.nav_hunk('next')
+              gitsigns.nav_hunk("next")
             end
           end)
 
-          map('n', '[c', function()
+          map("n", "[c", function()
             if vim.wo.diff then
-              vim.cmd.normal({'[c', bang = true})
+              vim.cmd.normal({ "[c", bang = true })
             else
-              gitsigns.nav_hunk('prev')
+              gitsigns.nav_hunk("prev")
             end
           end)
 
-          map('n', '<leader>gph', gitsigns.preview_hunk, {
-            desc = '[G]it [P]review [H]unk'
+          map("n", "<leader>gph", gitsigns.preview_hunk, {
+            desc = "[G]it [P]review [H]unk",
           })
 
-          map('n', '<leader>gb', gitsigns.blame,  {
-            desc = '[G]it [B]lame'
+          map("n", "<leader>gb", gitsigns.blame, {
+            desc = "[G]it [B]lame",
           })
-      end,
-    })
-  end,
-}
+        end,
+      })
+    end,
+  },
 }
