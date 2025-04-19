6995afb05beffec1d5060508653809ba62d8180b
tomskopek <tomskopek@gmail.com>
2025-04-19 08:17:01 -0700

    (setup) wip symlinks

---
+++ setup_symlinks.sh
@@ -0,0 +1,6 @@
+ln -s ~/dev/dotfiles/.hammerspoon ~/.hammerspoon
+ln -s ~/dev/dotfiles/karabiner.edn ~/.config/karabiner.edn
+
+
+# is this needed?
+# ln -s ~/dev/dotfiles/lazygit/config.yml ~/Library/Application\ Support/lazygit/config.yml
