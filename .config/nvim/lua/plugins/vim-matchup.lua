54c41c82a81ac8b924cf0de9066eac6b248eaf6f
Tom Skopek <tomskopek@gmail.com>
2025-12-04 14:07:28 -0500

    (vim) add vim-matchup

---
+++ .config/nvim/lua/plugins/vim-matchup.lua
@@ -0,0 +1,8 @@
+-- Highlihgt, navigate, and operate on matching text (eg: html tags, if/else/endif, etc)
+return {
+  "andymass/vim-matchup",
+  event = "BufReadPost",
+  enabled = true,
+  opts = {
+  }
+}
