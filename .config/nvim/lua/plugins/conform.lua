140950769480e5b87473db9236f45fb227c33d82
tomskopek <tomskopek@gmail.com>
2025-12-25 22:43:29 -0500

    (vim) prettier for json/yaml

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -4,11 +4,14 @@ return {
     opts = {
       formatters_by_ft = {
         lua = { "stylua" },
-        python = { "ruff_format", "isort" }, -- Conform will run multiple formatters sequentially
-        javascript = { "prettierd", "prettier", stop_after_first = true }, -- Conform will run the first available formatter
+        python = { "ruff_format" }, -- ruff_format handles both formatting and import sorting
+        javascript = { "prettierd", "prettier", stop_after_first = true },
         typescript = { "prettierd", "prettier", stop_after_first = true },
         javascriptreact = { "prettierd", "prettier", stop_after_first = true },
         typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+        json = { "prettierd", "prettier", stop_after_first = true },
+        yaml = { "prettierd", "prettier", stop_after_first = true },
+        markdown = { "prettierd", "prettier", stop_after_first = true },
       },
     },
     keys = {
