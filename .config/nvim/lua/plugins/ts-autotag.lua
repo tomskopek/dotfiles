4a4ddc62f2d627a1d31a91f6c075f7bdc992422c
tomskopek <tomskopek@gmail.com>
2026-02-14 22:32:47 -0500

    (vim) enable nvim-ts-autotag for closing matching tags; update opts to new format (opts.opts)

--- .config/nvim/lua/plugins/ts-autotag.lua
+++ .config/nvim/lua/plugins/ts-autotag.lua
@@ -1,11 +1,13 @@
 -- Use treesitter to autoclose and autorename html tags
 return {
   "windwp/nvim-ts-autotag",
-  enabled = false,
+  enabled = true,
   event = "VimEnter",
   opts = {
-    enable_close = true, -- Auto close tags
-    enable_rename = true, -- Auto rename pairs of tags
-    enable_close_on_slash = true -- Auto close on trailing </
-  }
+    opts = {
+      enable_close = true, -- Auto close tags
+      enable_rename = true, -- Auto rename pairs of tags
+      enable_close_on_slash = true, -- Auto close on trailing </
+    },
+  },
 }
