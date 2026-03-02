0044bce3ffb6f968969d69ff3cd5c17bb461cc2a
ideotom <tom@ideogram.ai>
2026-03-01 21:30:11 -0500

    (vim) editorconfig fix again (better fix)

--- .config/nvim/after/ftplugin/python.lua
+++ .config/nvim/after/ftplugin/python.lua
@@ -1,14 +1,16 @@
--- Only set these when editorconfig hasn't specified indent_size.
--- Context: if a project has a .editorconfig, there can sometimes be a race condition
---   where these settings load and override the .editorconfig.
---   .editorconfig should always take precedence. ie. if a project
---   specifies spacing of 2, we should always use that!
-local ec = vim.b.editorconfig or {}
-if not ec.indent_size then
-  vim.opt_local.shiftwidth = 4
-  vim.opt_local.tabstop = 4
-  vim.opt_local.softtabstop = 4
-end
+-- The built-in ftplugin/python.vim sets shiftwidth=4 (PEP 8 default).
+-- If a project has a .editorconfig, we need to re-apply its values here
+-- because the built-in ftplugin (4) can sometimes run AFTER editorconfig and overwrites them.
+-- Then this is the last thing that is run.
+-- Useful debugging commands:
+-- :verbose set shiftwidth? — tells you exactly which script last set a value
+-- :echo vim.b.editorconfig — confirms what editorconfig parsed
+local ec = vim.b.editorconfig or {}  -- get editorconfig
+local default_size = 4  -- I like indent of 4 personally, if it is not defined at the project level
+local size = tonumber(ec.indent_size) or default_size
+vim.opt_local.shiftwidth = size
+vim.opt_local.tabstop = size
+vim.opt_local.softtabstop = size
 
 vim.opt_local.colorcolumn = "100"
 vim.opt_local.foldmethod = "indent"
