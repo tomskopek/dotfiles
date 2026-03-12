e893e463825b9dec21ad4096fac21076db548323
ideotom <tom@ideogram.ai>
2026-03-12 10:36:19 -0400

    (nvim) improve nvimtree quit when last buffer

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -19,27 +19,19 @@ return {
         },
       })
 
-      -- Auto-close NvimTree when it's the last window.
-      -- Source: https://github.com/nvim-tree/nvim-tree.lua/wiki/Auto-Close
-      vim.api.nvim_create_autocmd("QuitPre", {
+      -- Auto-close NvimTree when it's the last non-floating window
+      vim.api.nvim_create_autocmd("BufEnter", {
+        nested = true,
         callback = function()
-          local tree_wins = {}
-          local floating_wins = {}
           local wins = vim.api.nvim_list_wins()
+          local non_floating = 0
           for _, w in ipairs(wins) do
-            local bufname = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(w))
-            if bufname:match("NvimTree_") ~= nil then
-              table.insert(tree_wins, w)
-            end
-            if vim.api.nvim_win_get_config(w).relative ~= "" then
-              table.insert(floating_wins, w)
+            if vim.api.nvim_win_get_config(w).relative == "" then
+              non_floating = non_floating + 1
             end
           end
-          if 1 == #wins - #floating_wins - #tree_wins then
-            -- Should quit, so we close all invalid windows.
-            for _, w in ipairs(tree_wins) do
-              vim.api.nvim_win_close(w, true)
-            end
+          if non_floating == 1 and vim.bo.filetype == "NvimTree" then
+            vim.cmd("quit")
           end
         end,
       })
