741d06d8d14e665fff2bcbab944bbde53ee81df9
ideotom <tom@ideogram.ai>
2026-05-07 12:14:00 -0400

    (vim) fix C-c for closing git blame

--- .config/nvim/lua/functions/close-git-blame.lua
+++ .config/nvim/lua/functions/close-git-blame.lua
@@ -1,7 +1,8 @@
 return function()
   for _, win in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
     local buf = vim.api.nvim_win_get_buf(win)
-    if vim.bo[buf].filetype == "gitsigns.blame" then
+    local ft = vim.bo[buf].filetype
+    if ft == "gitsigns-blame" or ft == "fugitiveblame" then
       vim.api.nvim_win_close(win, true)
       return true
     end
