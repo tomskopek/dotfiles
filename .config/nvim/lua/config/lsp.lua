ee336d709ab20a4c0320df16933f3587cfc1cbc9
ideotom <tom@ideogram.ai>
2026-05-28 14:49:46 -0400

    add lsp_openapi

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -1,6 +1,7 @@
 vim.lsp.enable({
   'basedpyright',
   'lua_ls',
+  'lsp_openapi',
   'ruff',
   'spectral',
   'ts_ls',
---
+++ .config/nvim/lua/plugins/lsp-openapi.lua
@@ -0,0 +1,6 @@
+return {
+  {
+    "tomskopek/lsp-openapi",
+    ft = "yaml",
+  },
+}
